6d2add744ff564e47dd12c9741ad868c5171a28f
bayne <nicholasgrubb@gmail.com>
2019-08-07 13:08:23 -0700

    facerecognizer changes

--- facerecognizer.py
+++ facerecognizer.py
@@ -12,14 +12,26 @@ KEY = '71ae76026ffc4bdfa8836cd950d2d66d'
 ENDPOINT_STRING = "westcentralus"
 ENDPOINT = 'https://{}.api.cognitive.microsoft.com/'.format(ENDPOINT_STRING)
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-video_capture = cv2.VideoCapture(0)
-# Check success
-if not video_capture.isOpened():
-   raise Exception("Could not open video device")
-# Read picture. ret === True on success
-ret, frame = video_capture.read()
-# Close device
-video_capture.release()
-detected_faces = face_client.face.detect_with_stream(frame)
-print(detected_faces)
-#cv2.imwrite('yeet.jpg', frame)
+
+face_list = face_client.face_list.create("42069boobies")
+
+count = 0
+while count < 11:
+   video_capture = cv2.VideoCapture(0)
+   # Check success
+   if not video_capture.isOpened():
+      raise Exception("Could not open video device")
+   # Read picture. ret === True on success
+   ret, frame = video_capture.read()
+   ret, frame = cv2.imencode('.jpg', frame)
+   # Close device
+   stream = io.BytesIO(frame)
+   face_client.face_list.add_face_from_stream("42069boobies")
+   video_capture.release()
+   count += 1
+
+
+cv2.imwrite('yeet.jpg', frame)
+
+detected_faces = face_client.face.detect_with_stream(stream, return_face_id=True)
+print(detected_faces[0].face_id)
