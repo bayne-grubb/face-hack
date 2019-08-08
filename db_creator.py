4fe733ddaa1e599146a245952f08585ccd24820f
bayne <nicholasgrubb@gmail.com>
2019-08-07 17:24:18 -0700

    facial recognition works now kinda

---
+++ db_creator.py
@@ -0,0 +1,16 @@
+import sqlite3
+from sqlite3 import Error
+import cv2
+import face_recognition
+
+
+print(male_face_encoding)
+"""try:
+    conn = sqlite3.connect()
+    print(sqlite3.version)
+    conn.cursor().execute("CREATE TABLE IF NOT EXISTS faces ( name text ")
+except Error as e:
+    print(e)
+finally:
+    conn.close()
+"""
--- facerecognizer.py
+++ facerecognizer.py
@@ -1,37 +1,134 @@
-import asyncio, io, glob, os, sys, time, uuid
-from urllib.parse import urlparse
-from azure.cognitiveservices.vision.face import FaceClient
-from msrest.authentication import CognitiveServicesCredentials
-from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 import cv2
-# Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
-# This key will serve all examples in this document.
-KEY = '71ae76026ffc4bdfa8836cd950d2d66d'
-# Set the API endpoint for your Face subscription.
-# You may need to change the first part ("westus") to match your subscription
-ENDPOINT_STRING = "westcentralus"
-ENDPOINT = 'https://{}.api.cognitive.microsoft.com/'.format(ENDPOINT_STRING)
-face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
-
-face_list_boi = face_client.face_list.create("7aeb69y2109")
-print(face_list_boi)
-count = 0
-while count < 11:
-   video_capture = cv2.VideoCapture(0)
-   # Check success
-   if not video_capture.isOpened():
-      raise Exception("Could not open video device")
-   # Read picture. ret === True on success
-   ret, frame = video_capture.read()
-   ret, frame = cv2.imencode('.jpg', frame)
-   # Close device
-   stream = io.BytesIO(frame)
-   face_client.face_list.add_face_from_stream("7aeb69y2109")
-   video_capture.release()
-   count += 1
+import face_recognition
+import sqlite3
 
 
-cv2.imwrite('yeet.jpg', frame)
+input_video = cv2.VideoCapture(0)
+peter_image = face_recognition.load_image_file("Image from iOS.jpg")
+peter_face_encoding = face_recognition.face_encodings(peter_image)[0]
 
-detected_faces = face_client.face.detect_with_stream(stream, return_face_id=True)
-print(detected_faces[0].face_id)
+
+bayne_image = face_recognition.load_image_file("IMG_20190807_162611.jpg")
+bayne_face_encoding = face_recognition.face_encodings(bayne_image)[0]
+
+
+faheem_image = face_recognition.load_image_file("faheem.jpg")
+faheem_face_encoding = face_recognition.face_encodings(faheem_image)[0]
+
+priya_image = face_recognition.load_image_file("priya.jpg")
+priya_face_encoding = face_recognition.face_encodings(priya_image)[0]
+
+rahul_image = face_recognition.load_image_file("rahul.jpg")
+rahul_face_encoding = face_recognition.face_encodings(rahul_image)[0]
+
+
+
+
+known_faces = [
+    peter_face_encoding,
+    bayne_face_encoding,
+    faheem_face_encoding,
+    priya_face_encoding,
+    rahul_face_encoding
+]
+
+
+face_locations = []
+face_encodings = []
+face_names = []
+
+
+def capture_and_encode_face():
+    count = 0
+    while count < 3:
+        ret, frame = input_video.read()
+        # Quit when the input video file ends
+        if not ret:
+            break
+        face_encodings = face_recognition.face_encodings(frame)
+        known_faces.append(face_encodings)
+
+
+def evaluate_faces():
+    while True:
+        # Grab a single frame of video
+        ret, frame = input_video.read()
+
+        # Quit when the input video file ends
+        if not ret:
+            break
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame[:, :, ::-1]
+
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            if face_encoding is None:
+                continue
+            match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+            if True in match:
+                continue
+            else:
+                return False
+
+
+def detect_and_name_face():
+    global face_locations, face_encodings, face_names
+    while True:
+        # Grab a single frame of video
+        ret, frame = input_video.read()
+
+        # Quit when the input video file ends
+        if not ret:
+            break
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame[:, :, ::-1]
+
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            if face_encoding is None:
+                continue
+            match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+
+            # If you had more than 2 faces, you could make this logic a lot prettier
+            # but I kept it simple for the demo
+            name = None
+            if match[0]:
+                name = "Peter"
+            elif match[1]:
+                name = "Bayne"
+
+            face_names.append(name)
+
+        # Label the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            if not name:
+                continue
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+        cv2.imshow('Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    input_video.release()
+    cv2.destroyAllWindows()
+
+
+detect_and_name_face()
